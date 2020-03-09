5e044c57a8c777347ec5e9a0f0cbcfa07945935c
Takahiro Mitsuoka <mono0423@gmail.com>
2020-03-09 12:19:29 +0900

    add  humidity measurement

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 require('./.env')
 import Obniz from 'obniz';
-
 const obniz = new Obniz(process.env.OBNIZ_ID);
+const lineNotify = require('line-notify-nodejs')(process.env.LINE_NOTIFY_TOKEN);
 
 obniz.connect();
-obniz.connectWait().then(() => {
-  console.log('connected');
+obniz.connectWait().then(async () => {
+  const sensor = obniz.wired("SHT31", { vcc: 0, sda: 1, scl: 2, adr: 3, gnd: 4, addressmode: 5 });
+
+  setInterval(async () => {
+    const data = await sensor.getAllWait();
+    console.log({ data });
+    
+    if (data.humidity < 40) {
+      await lineNotify.notify({
+        message: '乾いたよ',
+      })
+    }
+  }, 60000);
 });
--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     OBNIZ_ID: string;
+    LINE_NOTIFY_TOKEN: string;
   }
 }
